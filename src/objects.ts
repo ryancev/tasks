4cc454044beab48d9c02f57ccb1d15e7f372d4ac
Ryan Evans <ryancev@udel.edu>
2022-02-21 11:16:04 -0500

    Completed toMarkdown function.

--- src/objects.ts
+++ src/objects.ts
@@ -77,7 +77,13 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    if (question.type === "short_answer_question") {
+        return `# ${question.name}\n${question.body}`;
+    } else {
+        return `# ${question.name}\n${question.body}\n- ${question.options.join(
+            "\n- "
+        )}`;
+    }
 }
 
 /**
