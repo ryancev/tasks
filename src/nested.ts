9bd0bf7f693f1cb03f90432fe229c2bd87e96c55
Ryan Evans <ryancev@udel.edu>
2022-02-22 15:04:05 -0500

    Completed sameType function

--- src/nested.ts
+++ src/nested.ts
@@ -177,7 +177,17 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    const types = questions.map((question: Question): string => {
+        return question.type;
+    });
+    if (
+        types.includes("short_answer_question") &&
+        types.includes("multiple_choice_question")
+    ) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /***
