4c1e086606fa69b2fa6dc4abff8d203bb61a7b91
Ryan Evans <ryancev@udel.edu>
2022-02-22 15:06:19 -0500

    Completed addNewQuestion function

--- src/nested.ts
+++ src/nested.ts
@@ -1,6 +1,7 @@
 import { idText } from "typescript";
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
+import { makeBlankQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
@@ -201,7 +202,8 @@ export function addNewQuestion(
     name: string,
     type: QuestionType
 ): Question[] {
-    return [];
+    const withAddedQuestion = [...questions, makeBlankQuestion(id, name, type)];
+    return withAddedQuestion;
 }
 
 /***
