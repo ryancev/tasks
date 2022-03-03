03d9bf4caa3b438464100dcf8e2139e4486f1d68
Ryan Evans <ryancev@udel.edu>
2022-03-02 21:01:32 -0500

    Fixed DoubleHalf component without removing funcs

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+interface numProps {
+    dhValue: number;
+    setDhValue: (newNum: number) => void;
+}
+
+function Doubler({ dhValue, setDhValue }: numProps): JSX.Element {
+    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+}
+
+function Halver({ dhValue, setDhValue }: numProps): JSX.Element {
+    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+}
+
 export function DoubleHalf(): JSX.Element {
     const [dhValue, setDhValue] = useState<number>(10);
-
     return (
         <div>
             <h3>Double Half</h3>
@@ -11,8 +23,8 @@ export function DoubleHalf(): JSX.Element {
                 The current value is: <span>{dhValue}</span>
             </div>
             <div>
-                <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>
-                <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>
+                <Doubler setDhValue={setDhValue} dhValue={dhValue}></Doubler>
+                <Halver setDhValue={setDhValue} dhValue={dhValue}></Halver>
             </div>
         </div>
     );
